895f13136adad4de8732ba83d3258d7376fb5df3
rjcbandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2021-08-09 01:08:49 +0800

    Created EMP TurretGhost and Power Generator turret

---
+++ Source/TOWERDEFENSE/PowerGeneratorTurret.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Turret.h"
+#include "PowerGeneratorTurret.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENSE_API APowerGeneratorTurret : public ATurret
+{
+	GENERATED_BODY()
+	
+};
